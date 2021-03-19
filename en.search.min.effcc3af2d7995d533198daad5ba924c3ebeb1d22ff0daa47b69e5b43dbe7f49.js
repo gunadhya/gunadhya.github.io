1143bfcdbc170af5aaa05fd6d4229f537474219a
gunadhya <6939749+gunadhya@users.noreply.github.com>
2021-03-20 01:23:32 +0530

    updated baseurl

--- en.search.min.effcc3af2d7995d533198daad5ba924c3ebeb1d22ff0daa47b69e5b43dbe7f49.js
+++ en.search.min.effcc3af2d7995d533198daad5ba924c3ebeb1d22ff0daa47b69e5b43dbe7f49.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/gunadhya/website.github.io/flexsearch.min.js');loadScript('/gunadhya/website.github.io/en.search-data.min.68fabe95bbb801752446da3f727fc64865b63816913e663d6bac32b442bd3415.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.c8a90061a67b0c2a90fee81a4c487d6b89c6100bc5a63f1e9b6dd71855b2aa19.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
